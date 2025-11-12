336dbb44c907259494281242363b7fed7107709c
DAMILOLA8909 <damsony.soji@gmail.com>
2025-11-12 21:49:00 +0100

    Add Header navigation and page routing setup

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Posts</h1>
-      <PostCard />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-8 bg-gray-50">
+        <h1 className="text-3xl font-bold mb-6 text-gray-800">Posts</h1>
+        <PostCard />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Users</h1>
-      <p>List of users will appear here.</p>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-8 bg-gray-50">
+        <h1 className="text-3xl font-bold mb-6 text-gray-800">Users</h1>
+        <p>List of users will appear here.</p>
+      </main>
     </div>
   );
 };
